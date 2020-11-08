90a530a6e924684674fe6cd58e2772ef124fce61
r0748973 <noa.andries@student.ucll.be>
2020-11-08 16:43:03 +0100

    fisheye

--- raytracer/raytracer/cameras/fisheye-camera.cpp
+++ raytracer/raytracer/cameras/fisheye-camera.cpp
@@ -1,44 +1,47 @@
-#include "cameras/perspective-camera.h"
+#include "cameras/fisheye-camera.h"
 #include "math/rectangle3d.h"
 #include <assert.h>
+#include "math/interval-mapper.h"
+#include "math/transformation-matrices.h"
 
 using namespace raytracer;
 using namespace raytracer::cameras;
 using namespace math;
 
 
-raytracer::cameras::_private_::PerspectiveCamera::PerspectiveCamera(const math::Matrix4x4 transformation, const Rectangle3D& view_window)
-    : DisplaceableCamera(transformation), m_view_window(view_window)
+raytracer::cameras::_private_::FisheyeCamera::FisheyeCamera(const Matrix4x4 transformation, Angle horizontal_angle, Angle vertical_angle)
+	: DisplaceableCamera(transformation), horizontal_angle(horizontal_angle), vertical_angle(vertical_angle)
 {
-    // NOP
+	// NOP
 }
 
-void raytracer::cameras::_private_::PerspectiveCamera::enumerate_untransformed_rays(const Point2D& point, std::function<void(const math::Ray&)> callback) const
+void raytracer::cameras::_private_::FisheyeCamera::enumerate_untransformed_rays(const Point2D& point, std::function<void(const math::Ray&)> callback) const
 {
-    assert(interval(0.0, 1.0).contains(point.x()));
-    assert(interval(0.0, 1.0).contains(point.y()));
+	const auto interval_horizontal = interval(
+		-(horizontal_angle.degrees() / 2) - 90,
+		(horizontal_angle.degrees() / 2) - 90
+	);
+	const auto interval_vertical = interval(
+		-(vertical_angle.degrees() / 2),
+		(vertical_angle.degrees() / 2)
+	);
+	const auto x_mapper = IntervalMapper<double, double>(interval(0.0, 1.0), interval_horizontal);
+	const auto y_mapper = IntervalMapper<double, double>(interval(0.0, 1.0), interval_vertical);
 
-    Point3D p = m_view_window.project(point);
+	const auto spherical_p = Point3D::spherical(1, Angle::degrees(x_mapper[point.x()]), Angle::degrees(y_mapper[point.y()]));
 
-    callback(Ray(math::Point3D(0, 0, 0), p));
+	callback(Ray(Point3D(0, 0, 0), spherical_p));
 }
 
-Camera raytracer::cameras::perspective(
-    const math::Point3D& eye,
-    const math::Point3D& look_at,
-    const math::Vector3D& up,
-    double distance,
-    double aspect_ratio)
+Camera raytracer::cameras::fisheye(
+	const math::Point3D& eye,
+	const math::Point3D& look_at,
+	const math::Vector3D& up,
+	const Angle horizontal_angle,
+	const Angle vertical_angle)
 {
-    assert(up.is_unit());
+	assert(up.is_unit());
+	Matrix4x4 transformation = _private_::create_transformation(eye, look_at, up);
 
-    double view_window_width = aspect_ratio;
-    double view_window_height = 1;
-    Point3D view_window_origin = Point3D(view_window_width / 2, -view_window_height / 2, distance);
-    Vector3D view_window_right = Vector3D(-view_window_width, 0, 0);
-    Vector3D view_window_up = Vector3D(0, view_window_height, 0);
-    Rectangle3D view_window(view_window_origin, view_window_right, view_window_up);
-    Matrix4x4 transformation = _private_::create_transformation(eye, look_at, up);
-
-    return Camera(std::make_shared<_private_::PerspectiveCamera>(transformation, view_window));
+	return Camera(std::make_shared<_private_::FisheyeCamera>(transformation, horizontal_angle, vertical_angle));
 }
--- raytracer/raytracer/cameras/fisheye-camera.h
+++ raytracer/raytracer/cameras/fisheye-camera.h
@@ -7,6 +7,7 @@
 #include "math/rectangle3d.h"
 #include <memory>
 
+using namespace math;
 
 namespace raytracer
 {
@@ -14,31 +15,26 @@ namespace raytracer
     {
         namespace _private_
         {
-            class PerspectiveCamera : public DisplaceableCamera
+            class FisheyeCamera : public DisplaceableCamera
             {
             public:
-                PerspectiveCamera(const math::Matrix4x4 transformation, const math::Rectangle3D&);
+                FisheyeCamera(const math::Matrix4x4 transformation, Angle horizontal_angle, Angle vertical_angle);
 
             protected:
                 void enumerate_untransformed_rays(const math::Point2D&, std::function<void(const math::Ray&)>) const;
 
             private:
-                math::Rectangle3D m_view_window;
+                Angle horizontal_angle;
+                Angle vertical_angle;
             };
         }
 
-        /// <summary>
-        /// Creates a perspective camera.
-        /// </summary>
-        /// <param name="eye">The camera's location</param>
-        /// <param name="look_at">The point the camera is directed at</param>
-        /// <param name="up">Specifies how the camera is tilted sideways. (0,1,0) is standard.</param>
-        /// <param name="distance">Distance between eye and view plane.</param>
-        Camera perspective(
+
+        Camera fisheye(
             const math::Point3D& eye,
             const math::Point3D& look_at,
             const math::Vector3D& up,
-            double distance,
-            double aspect_ratio);
+            Angle horizontal_angle,
+            Angle vertical_angle);
     }
 }
