3bbdc0fac9909f925c84a25418d5be7782a9409f
Kasper Vanderhulst <kasper.vanderhulst@student.ucll.be>
2020-11-27 14:05:26 +0100

    lissajous

---
+++ raytracer/raytracer/animation/lissajous.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "animation/animation.h"
+
+namespace animation
+{
+	Animation<math::Point3D> lissajous_animation(animation::Duration duration, const double& xAmp, const double& yAmp, const double& zAmp, const double& xFreq,
+		const double& yFreq, const double& zFreq, math::Angle& xPhase, math::Angle& yPhase, math::Angle& zPhase);
+}
